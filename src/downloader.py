5b13c982fd9c6f16f8887399b3f71cd7e40dfe38
Bishop <186253171+webdev-one@users.noreply.github.com>
2024-12-25 01:46:48 +0800

    Add endpoints

--- src/downloader.py
+++ src/downloader.py
@@ -3,3 +3,6 @@ from pathlib import Path
 
 import requests
 
+puzzles_endpoint = "https://edge.games.nyti.nyt.net/svc/crosswords/v3/35806626/puzzles.json"
+puzzle_print_url_format = f"https://www.nytimes.com/svc/crosswords/v2/puzzle/print/{_}.pdf"
+puzzle_url_format = f"https://www.nytimes.com/svc/crosswords/v2/puzzle/{_}.pdf"
