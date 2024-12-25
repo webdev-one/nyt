bf77d95d162e93bef5f6aefe0d3df2808e3b3aee
Bishop <186253171+webdev-one@users.noreply.github.com>
2024-12-25 11:34:26 +0800

    Remove unnecessary print calls

--- src/downloader.py
+++ src/downloader.py
@@ -1,7 +1,7 @@
 from datetime import date
 from http.cookiejar import MozillaCookieJar
 from pathlib import Path
-from pprint import pprint
+# from pprint import pprint
 
 import requests
 
@@ -27,18 +27,18 @@ def download(cookies_path: str = "cookies.txt"):
 
         #Set source filename
         source_filename=date.today().strftime("%b%d%y")
-        pprint(source_filename)
+        # pprint(source_filename)
 
         # Set destination path and check for existing
         dest_filename = f"{date.today().year}{date.today().month}{date.today().day}.pdf"
         dest_path = f"{out_dir}/{dest_filename}"
         if Path(dest_path).exists():
-            pprint(f"{dest_path} already downloaded.")
+            print(f"{dest_path} already downloaded.")
 
         # Download the puzzle file
         puzzle = session.get(puzzle_print_url_format % source_filename)
         if puzzle.status_code != 200:
-            pprint("Request error.")
+            print("Request error.")
         else:
-            print(dest_path)
+            # print(dest_path)
             Path(dest_path).write_bytes(puzzle.content)
