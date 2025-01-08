9d55038b757ec87a91d7c38b53b47cde5a30d7d3
Bishop <186253171+webdev-one@users.noreply.github.com>
2025-01-08 10:07:22 +0800

    Fetch online version of crossword instead of print version

--- src/downloader.py
+++ src/downloader.py
@@ -3,21 +3,29 @@ from http.cookiejar import MozillaCookieJar
 from pathlib import Path
 # from pprint import pprint
 
+import logging
 import requests
 
 
-# puzzles_endpoint = "https://www.nytimes.com/svc/crosswords/v3/puzzles.json"
-puzzle_print_url_format = "https://www.nytimes.com/svc/crosswords/v2/puzzle/print/%s.pdf"
-# puzzle_url_format = f"https://www.nytimes.com/svc/crosswords/v2/puzzle/{_}.pdf"
+DEBUG = False
 
 
-def download(cookies_path: str = "cookies.txt"):
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
+
+
+puzzles_endpoint = "https://www.nytimes.com/svc/crosswords/v3/puzzles.json"
+# puzzle_print_url_format = "https://www.nytimes.com/svc/crosswords/v2/puzzle/print/%s.pdf"
+puzzle_url_format = "https://www.nytimes.com/svc/crosswords/v2/puzzle/%s.pdf"
+
+
+def download(cookies_path: str = "cookies/cookies.txt"):
     # params = {}
     # out_dir = f"{date.today().year}/{date.today().month}"
     Path("output").mkdir(parents=True, exist_ok=True)
 
-    # response = requests.get(puzzles_endpoint, params).json()
-    # pprint(response)
+    response = requests.get(puzzles_endpoint).json()["results"]
+    logging.debug(f"{response[0]=}")
 
     with requests.Session() as session:
         # Initiate session
@@ -25,20 +33,37 @@ def download(cookies_path: str = "cookies.txt"):
         cookies.load()
         session.cookies = cookies
 
-        #Set source filename
-        source_filename=date.today().strftime("%b%d%y")
-        # pprint(source_filename)
+        # Set target date
+        filename = date.today().strftime("%Y-%m-%d")
+        logging.debug(f"{filename=}")
+
+        # Check puzzle publication
+        if response:
+            if response[0]["print_date"] == filename:
+                puzzle_id = response[0]["puzzle_id"]
+                logging.debug(f"{puzzle_id=}")
+            else:
+                print(f"Current puzzle is {response[0]["print_date"]}. Check again later.")
+                exit()
+        else:
+            print("No valid response received from JSON.")
+            exit()
+
+        # Set target file URL
+        source_filename = puzzle_url_format % puzzle_id
+        logging.debug(f"{source_filename=}")
 
         # Set destination path and check for existing
-        dest_filename = f"{date.today().year}{date.today().month}{date.today().day}.pdf"
-        dest_path = f"{"output"}/{dest_filename}"
+        dest_path = f"{"output"}/{filename}.pdf"
         if Path(dest_path).exists():
             print(f"{dest_path} already downloaded.")
+            exit()
 
         # Download the puzzle file
-        puzzle = session.get(puzzle_print_url_format % source_filename)
+        puzzle = session.get(source_filename)
         if puzzle.status_code != 200:
             print("Request error.")
         else:
             # print(dest_path)
             Path(dest_path).write_bytes(puzzle.content)
+            print(f"Downloaded to {dest_path}.")
